ef0971cc552a6c8415d1930c372d6f6796c71596
IanDesuyo <59338745+IanDesuyo@users.noreply.github.com>
2022-03-28 10:39:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# AfkXreading
+
+Afk script for xreading.
+
+## How to use
+
+1. Install Tampermonkey or any other script manager.
+2. Install [AfkXreading](https://raw.githubusercontent.com/IanDesuyo/AfkXreading/master/main.js) from GitHub.
+3. Set the WPM (words per minute) in the script.
+4. Open a book and wait :tada:
+
+## Known issues
+
+* Does not work when the page is not in the foreground.
---
+++ main.js
@@ -0,0 +1,86 @@
+// ==UserScript==
+// @name         AfkXreading
+// @version      0.1
+// @description  Afk script for xreading.
+// @author       IanDesuyo
+// @match        https://xreading.com/local/reader/index.php*
+// @grant        none
+// ==/UserScript==
+
+const wordsPerMinute = 150;
+
+async function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function continueReadingObserver() {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      if (mutation.attributeName === "class" && mutation.target.classList.contains("show")) {
+        console.log("Continue Reading detected, timeout 1s...");
+        setTimeout(() => {
+          document.querySelector(".continue-reading").click();
+          console.log("Continue Reading clicked");
+        }, 1000);
+      }
+    });
+  });
+  observer.observe(document.querySelector("#popupidletimeout"), { attributes: true });
+
+  return observer;
+}
+
+async function getBookStatus() {
+  const currentBookId = new URLSearchParams(window.location.search).get("cid");
+
+  const sesskey = await fetch("https://xreading.com/blocks/institution/mybooks.php?tm=mybooks")
+    .then(res => res.text())
+    .then(text => /"sesskey":"(.*?)"/gs.exec(text)[1]);
+
+  const resp = await fetch(
+    `https://xreading.com/blocks/institution/ajax/request.php?sesskey=${sesskey}&action=mybookdata`
+  ).then(res => res.json());
+
+  const books = resp.html.match(/(<tr class="">.*?<\/tr>)+/gs);
+
+  for (let i = 0; i < books.length; i++) {
+    const book = books[i];
+
+    const bookReg =
+      /<div class="book-shortinfo" cid="(\d+?)">.*?<span.*?class="user-book-title">(.*?)<\/span>.*?<span>Words: (.*?), .*?<td class=".*?creadtime".*?>([\d:]+)<\/td>.*?<td class=".*?creadingspeed".*?>([\d.]+)<\/td>/gs.exec(
+        book
+      );
+
+    if (bookReg && bookReg[1] === currentBookId) {
+      return {
+        id: parseInt(bookReg[1]),
+        title: bookReg[2],
+        totalWords: parseInt(bookReg[3].replace(/,/g, "")),
+        currentReadTime: bookReg[4],
+        speed: parseFloat(bookReg[5]),
+      };
+    }
+  }
+}
+
+(async function () {
+  const observer = continueReadingObserver();
+  const bookStatus = await getBookStatus();
+
+  await delay(5000); // wait for the page to load
+  while (true) {
+    const words = document
+      .querySelector(".ajax-content.reader-book-title .activeContent")
+      .textContent.split(" ")
+      .filter(x => x.length > 0);
+
+    console.log(`${words.length} words in this page`);
+    const waitTime = (words.length / wordsPerMinute) * 60;
+    console.log(`Waiting ${waitTime} seconds...`);
+    await delay(waitTime * 1000);
+    document.querySelector(".btn.next-slide").click();
+    await delay(5000); // wait for the next page to load
+  }
+})();
+// You can open the continue reading popup by using the following script:
+//  $("#popupidletimeout").modal({ backdrop: "static", keyboard: !1 });
